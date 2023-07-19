ef01fbe9365d4f8fa0b7d76ee6cebe8633df958a
Eugene <unegee0@gmail.com>
2023-07-20 00:55:20 +0500

    c-1

--- dz-4/Program.cs
+++ dz-4/Program.cs
@@ -1,6 +1,6 @@
 ﻿/*Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. (можно указать любой промежуток)
 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
-6, 1, 33 -> [6, 1, 33]*/
+6, 1, 33 -> [6, 1, 33]
 
 int[] getArray(int size)
 {
@@ -13,4 +13,42 @@ return result;
 }
 
 int[] array = getArray(8);
-Console.WriteLine($"[{String.Join(",",array)}]");
+Console.WriteLine($"[{String.Join(",",array)}]");*/
+
+/*Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+
+int x = ReadInt("Enter to number: ");
+
+int lena = NumberLen(x);
+SumNumbers(x, lena);
+
+int NumberLen(int y)
+{
+    int index = 0;
+    while (y > 0)
+    {
+        y /= 10;
+        index++;
+    }
+    return index;
+}
+
+void SumNumbers(int z, int lena)
+{
+    int sum = 0;
+    for (int i = 1; i <= lena; i++)
+    {
+        sum += z % 10;
+        z /= 10;
+    }
+    Console.WriteLine($"Summ numeral in number {sum}: ");
+}
+
+int ReadInt(string window)
+{
+    Console.Write(window);
+    return Convert.ToInt32(Console.ReadLine());
+}*/
