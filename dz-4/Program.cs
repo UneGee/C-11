3d52c29dddd8bc033c05f3fddce156b28aa6b53c
Eugene <unegee0@gmail.com>
2023-07-20 01:07:42 +0500

    c-1

--- dz-4/Program.cs
+++ dz-4/Program.cs
@@ -52,3 +52,19 @@ int ReadInt(string window)
     Console.Write(window);
     return Convert.ToInt32(Console.ReadLine());
 }*/
+
+/*Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16*/
+
+Console.WriteLine("Enter to number A: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter to number B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int step = a;
+
+for (int i = 1; i < b; i++)
+{
+    step = step * a;
+}
+Console.WriteLine($"A to the extent of B: " + step);
